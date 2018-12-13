5f629f1be5b6cb6317c488887593ef9ad78c12bb
Joshua Henderson <joshua.henderson@microchip.com>
2018-12-12 18:38:46 -0700

    split basic example into basic and basicui examples
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/basicui/basicui.cpp
+++ examples/basicui/basicui.cpp
@@ -4,15 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <egt/ui>
+#include <egt/uiloader.h>
+
+#define SHARED_PATH "../share/egt/examples/basicui/"
 
 int main(int argc, const char** argv)
 {
     egt::Application app(argc, argv);
 
-    egt::TopWindow window;
-    egt::Button button(window, "Press Me");
-    button.set_align(egt::alignmask::CENTER);
-    window.show();
+    egt::experimental::UiLoader loader;
+    auto window = loader.load(SHARED_PATH "ui.xml");
+    window->show();
 
     return app.run();
 }
