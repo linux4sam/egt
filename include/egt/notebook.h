1e974074dd1f94861086962bc7593aa3302dd5d8
Joshua Henderson <joshua.henderson@microchip.com>
2019-01-15 13:56:45 -0700

    add Notebook widget
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ include/egt/notebook.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef EGT_NOTEBOOK_H
+#define EGT_NOTEBOOK_H
+
+/**
+ * @file
+ * @brief Working with notebooks.
+ */
+
+#include <egt/frame.h>
+#include <vector>
+
+namespace egt
+{
+    inline namespace v1
+    {
+        /**
+         * A single tab in a Notebook.
+         */
+        class NotebookTab : public Frame
+        {
+        public:
+
+            /**
+             * @return true if allowed to leave, false otherwise.
+             */
+            virtual bool leave()
+            {
+                return true;
+            }
+
+            /**
+             * Called when this tab is entered and before show() is invoked.
+             */
+            virtual void enter()
+            {
+            }
+
+            virtual ~NotebookTab()
+            {}
+        };
+
+        /**
+         * Allows a collection of NotebookTab widgets to be shown one at a time.
+         */
+        class Notebook : public Frame
+        {
+        public:
+            explicit Notebook(const Rect& rect = Rect());
+
+            virtual NotebookTab* add(NotebookTab* widget);
+
+            virtual void remove(Widget* widget) override;
+
+            virtual void set_select(uint32_t index);
+
+            virtual ~Notebook();
+
+        protected:
+
+            struct Cell
+            {
+                // cppcheck-suppress unusedStructMember
+                NotebookTab* widget{nullptr};
+                std::string name;
+            };
+
+            using cell_array = std::vector<Cell>;
+
+            cell_array m_cells;
+
+            int m_current_index{-1};
+
+        private:
+            virtual Widget* add(Widget* widget) override
+            {
+                ignoreparam(widget);
+                return nullptr;
+            }
+
+        };
+
+    }
+}
+
+#endif
