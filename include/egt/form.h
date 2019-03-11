a1b45ca344c825fcfaf597447d6e6472fb839334
Joshua Henderson <joshua.henderson@microchip.com>
2019-03-11 08:27:14 -0700

    more work on Form class
    Bring back to a compiling state and fix several issues
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- include/egt/form.h
+++ include/egt/form.h
@@ -14,6 +14,7 @@
 #include <egt/frame.h>
 #include <egt/grid.h>
 #include <egt/sizer.h>
+#include <memory>
 
 namespace egt
 {
@@ -24,64 +25,94 @@ namespace experimental
 {
 
 /**
- * Help with the creation is standard layout forms.
+ * Help with the creation is standard for layouts.
  *
  */
 class Form : public Frame
 {
 public:
 
-    Form(const std::string& title)
+    explicit Form(const std::string& title = std::string()) noexcept
         : Frame(),
           m_vsizer(orientation::vertical)
     {
-        add(&vsizer);
+        set_name("Form" + std::to_string(m_widgetid));
+
+        m_vsizer.set_align(alignmask::expand);
+        add(m_vsizer);
 
         if (!title.empty())
         {
-            auto label = make_shared<Label>(caption);
-            m_store.push_back(label);
-
+            auto label = std::make_shared<Label>(title,
+                                                 alignmask::center,
+                                                 Font(Font::weightid::BOLD));
+            label->font().size(label->font().size() + 5);
             label->set_align(alignmask::expand_horizontal);
             m_vsizer.add(label);
         }
     }
 
-    void add_group(const std::string& caption)
+    explicit Form(Frame& parent, const std::string& title = std::string()) noexcept
+        : Form(title)
     {
-        auto label = make_shared<Label>(caption);
-        m_store.push_back(label);
+        parent.add(*this);
+    }
 
-        label->font().size(label->font().size() + 5);
-        label->font().weight(weightid::BOLD);
-        label->set_align(alignmask::expand);
+    virtual void add_group(const std::string& caption)
+    {
+        auto label = std::make_shared<Label>(caption);
+        label->font().weight(Font::weightid::BOLD);
+        label->set_align(alignmask::expand_horizontal);
+        label->set_text_align(alignmask::bottom | alignmask::left);
         m_vsizer.add(label);
+        m_options.push_back(label.get());
     }
 
-    void add_option(const std::string& name, Widget& widget)
+    template<class T>
+    void add_option(const std::string& name, const std::shared_ptr<T>& widget)
     {
-        widget->set_align(alignmask::expand);
-
-        auto label = make_shared<Label>(name);
-        m_store.push_back(label);
-
-        auto grid = new StaticGrid(Rect(Point(), Size(0, 50), 1, 2);
-                                   grid->set_align(alignmask::expand_horizontal);
-                                   grid->add(label);
-                                   grid->add(widget);
-                                   m_vsizer.add(grid);
+        auto p = std::dynamic_pointer_cast<Widget>(widget);
+        add_option(name, p);
     }
 
-    void add_option(Widget& widget)
+    virtual void add_option(const std::string& name, const std::shared_ptr<Widget>& widget)
     {
         widget->set_align(alignmask::expand);
-        m_vsizer.add(widget);
+        auto label = std::make_shared<Label>(name);
+        label->set_align(alignmask::expand);
+        label->set_text_align(alignmask::center | alignmask::left);
+        auto grid = std::make_shared<StaticGrid>(Tuple(2, 1));
+        grid->set_margin(2); /// @todo Not working
+        grid->resize(Size(0, 20));
+        grid->set_align(alignmask::expand_horizontal);
+        grid->add(label);
+        grid->add(widget);
+        m_vsizer.add(grid);
+        m_options.push_back(grid.get());
+    }
+
+    virtual void add_option(const std::shared_ptr<Widget>& widget)
+    {
+        widget->set_align(alignmask::expand);
+        auto grid = std::make_shared<StaticGrid>(Tuple(1, 1));
+        grid->set_margin(2); /// @todo Not working
+        grid->resize(Size(0, 20));
+        grid->set_align(alignmask::expand_horizontal);
+        grid->add(widget);
+        m_vsizer.add(grid);
+        m_options.push_back(grid.get());
+    }
+
+    virtual void remove_options()
+    {
+        for (auto option : m_options)
+            m_vsizer.remove(option);
     }
 
 protected:
 
     BoxSizer m_vsizer;
-    std::vector<std::shared_ptr<Widget>> m_store;
+    std::vector<Widget*> m_options;
 };
 
 }
