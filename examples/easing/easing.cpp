b37ffcae3d444207aa4b8397be3d0b037edc6f3a
Joshua Henderson <joshua.henderson@microchip.com>
2019-04-09 08:25:20 -0700

    cleanup and simplify easing example
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/easing/easing.cpp
+++ examples/easing/easing.cpp
@@ -3,80 +3,44 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include <egt/ui>
-#include <math.h>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
-#include <sstream>
-#include <iostream>
-#include <random>
-#include <cmath>
 
 using namespace std;
 using namespace egt;
 
-static easing_func curves[] =
+static vector<pair<easing_func, string>> easing_functions =
 {
-    easing_linear,
-    easing_easy,
-    easing_easy_slow,
-    easing_extend,
-    easing_drop,
-    easing_drop_slow,
-    easing_snap,
-    easing_bounce,
-    easing_bouncy,
-    easing_rubber,
-    easing_spring,
-    easing_boing,
-    easing_quadratic_easeinout,
-    easing_cubic_easeinout,
-    easing_quartic_easeinout,
-    easing_quintic_easeinout,
-    easing_sine_easeinout,
-    easing_circular_easeinout,
-    easing_exponential_easeinout,
-};
-
-static vector<std::string> curve_names =
-{
-    "linear",
-    "easy",
-    "easy slow",
-    "extend",
-    "drop",
-    "drop slow",
-    "snap",
-    "bounce",
-    "bouncy",
-    "rubber",
-    "spring",
-    "boing",
-    "quadratic",
-    "cubic",
-    "quartic",
-    "quintic",
-    "sine",
-    "circular",
-    "exponential",
+    {easing_linear, "linear"},
+    {easing_easy, "easy"},
+    {easing_easy_slow, "easy slow"},
+    {easing_extend, "extend"},
+    {easing_drop, "drop"},
+    {easing_drop_slow, "drop slow"},
+    {easing_snap, "snap"},
+    {easing_bounce, "bounce"},
+    {easing_bouncy, "bouncy"},
+    {easing_rubber, "rubber"},
+    {easing_spring, "spring"},
+    {easing_boing, "boing"},
+    {easing_quadratic_easeinout, "quadratic"},
+    {easing_cubic_easeinout, "cubic"},
+    {easing_quartic_easeinout, "quartic"},
+    {easing_quintic_easeinout, "quintic"},
+    {easing_sine_easeinout, "sine"},
+    {easing_circular_easeinout, "circular"},
+    {easing_exponential_easeinout, "exponential"}
 };
 
 class MainWindow : public TopWindow
 {
 public:
     MainWindow()
-        : TopWindow(),
-          m_seq(true),
+        : m_seq(true),
           m_animation(-110, h() - 100, std::chrono::seconds(2)),
           m_delay(std::chrono::seconds(1))
-    {}
-
-    int load()
     {
         auto i = Image("background.png");
         auto img = make_shared<ImageLabel>(i);
@@ -85,48 +49,40 @@ public:
         img->scale_image(scale);
 
         ListBox::item_array items;
-        items.resize(curve_names.size());
-        transform(curve_names.begin(), curve_names.end(), items.begin(),
-        [](const std::string & v) { return make_shared<StringItem>(v);});
+        items.resize(easing_functions.size());
+        transform(easing_functions.begin(), easing_functions.end(), items.begin(),
+        [](const std::pair<easing_func, string>& v) { return make_shared<StringItem>(v.second);});
         auto list1 = make_shared<ListBox>(items, Rect(Point(0, 0), Size(150, 0)));
         list1->set_align(alignmask::expand_vertical | alignmask::right);
         add(list1);
+        list1->set_select(7);
 
         list1->on_event([this, list1](eventid)
         {
             m_seq.reset();
-            m_animation.set_easing_func(curves[list1->selected()]);
+            m_animation.set_easing_func(easing_functions[list1->selected()].first);
             m_seq.start();
             return 0;
         }, {eventid::property_changed});
 
         auto image = std::make_shared<ImageLabel>(Image("ball.png"));
-        // There is a bug on 9x5 that if the plane is all the way out of view
-        // then it will cause glitches. So, we create the height (which will
-        // be invisible), to always keep a portion of the plane on screen
-        // alternate of making the plane the same exact size as the image.
+        image->set_boxtype(Theme::boxtype::none);
+
         m_box = make_shared<Window>(Size(100, 200));
         m_box->set_boxtype(Theme::boxtype::none);
         m_box->add(image);
         m_box->move(Point(w() / 2 - m_box->w() / 2, -110));
         m_box->show();
-
         add(m_box);
 
         m_animation.on_change(std::bind(&Window::set_y, m_box, std::placeholders::_1));
         m_seq.add(m_animation);
         m_seq.add(m_delay);
-
-        list1->set_select(7);
-
         m_seq.start();
-
-        return 0;
     }
 
 private:
     shared_ptr<Window> m_box;
-
     AnimationSequence m_seq;
     PropertyAnimator m_animation;
     AnimationDelay m_delay;
@@ -137,8 +93,6 @@ int main(int argc, const char** argv)
     Application app(argc, argv, "easing");
 
     MainWindow window;
-
-    window.load();
 
     Label label1("CPU: 0%",
                  Rect(Point(10, window.size().h - 40),
