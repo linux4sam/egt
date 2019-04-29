23cb42a7ca1d513c754cd3e16294c27feb14efd6
Joshua Henderson <joshua.henderson@microchip.com>
2019-04-29 15:58:21 -0700

    update i18n example to show multiple languages
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/i18n/i18n.cpp
+++ examples/i18n/i18n.cpp
@@ -4,14 +4,38 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <egt/ui>
+#include <string>
+
+using namespace std;
+using namespace egt;
 
 int main(int argc, const char** argv)
 {
-    egt::Application app(argc, argv, "i18n");
+    Application app(argc, argv, "i18n");
 
-    egt::TopWindow window;
-    egt::Button button(window, gettext("Hello World"));
-    button.set_align(egt::alignmask::center);
+    TopWindow window;
+
+    VerticalBoxSizer vsizer;
+    expand(vsizer);
+
+    vector<string> variations =
+    {
+        _("EGT supports all languages"),
+        "EGT支持所有語言",
+        "EGT prend en charge toutes les langues",
+        "EGT unterstützt alle Sprachen",
+        "EGT soporta todos los idiomas",
+        _("Hello World")
+    };
+
+    for (auto& str : variations)
+    {
+        static const auto face = "Noto Sans CJK SC Regular";
+        auto label = make_shared<Label>(str, egt::Label::default_align, egt::Font(face, 36));
+        vsizer.add(label);
+    }
+
+    window.add(vsizer);
     window.show();
 
     return app.run();
