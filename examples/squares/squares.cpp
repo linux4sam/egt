737d12571e97e4281575ebdbe9515759ee3e3bc8
Joshua Henderson <joshua.henderson@microchip.com>
2019-05-29 16:34:15 -0700

    add some variation to the squares example
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/squares/squares.cpp
+++ examples/squares/squares.cpp
@@ -18,19 +18,34 @@ int main(int argc, const char** argv)
 
     const Color FUCHSIA(Color::CSS("#F012BE"));
 
+    VerticalBoxSizer sizer;
+
     Label label("FPS: ---");
     label.set_color(Palette::ColorId::text, Palette::black);
     label.set_color(Palette::ColorId::bg, Palette::transparent);
+    sizer.add(expand_horizontal(label));
+
+    Label label_dims("");
+    label_dims.set_color(Palette::ColorId::text, Palette::black);
+    label_dims.set_color(Palette::ColorId::bg, Palette::transparent);
+    sizer.add(expand_horizontal(label_dims));
 
     Popup popup(Size(100, 80));
     popup.set_color(Palette::ColorId::bg, FUCHSIA);
-    popup.add(label);
+    popup.add(sizer);
+    expand(sizer);
     top(right(popup));
     win.add(popup);
     popup.show();
 
-    default_dim_type WIDTH = 100;
-    default_dim_type HEIGHT = 100;
+    default_dim_type width = 100;
+    default_dim_type height = 100;
+
+    {
+        ostringstream ss;
+        ss << width << "," << height;
+        label_dims.set_text(ss.str());
+    }
 
     const vector<Color> colors =
     {
@@ -55,8 +70,8 @@ int main(int argc, const char** argv)
 
     std::random_device r;
     std::default_random_engine e1 {r()};
-    std::uniform_real_distribution<float> x_dist {0., static_cast<float>(win.w() - WIDTH)};
-    std::uniform_real_distribution<float> y_dist {0., static_cast<float>(win.h() - HEIGHT)};
+    std::uniform_real_distribution<float> x_dist {0., static_cast<float>(win.w() - width)};
+    std::uniform_real_distribution<float> y_dist {0., static_cast<float>(win.h() - height)};
     std::uniform_int_distribution<int> color_dist {0, static_cast<int>(colors.size()) - 1};
 
     experimental::Fps fps;
@@ -65,7 +80,7 @@ int main(int argc, const char** argv)
     PeriodicTimer timer(std::chrono::milliseconds(1));
     timer.on_timeout([&]()
     {
-        Rect rect(x_dist(e1), y_dist(e1), WIDTH, HEIGHT);
+        Rect rect(x_dist(e1), y_dist(e1), width, height);
 
         Painter painter(win.screen()->context());
         painter.set(colors[color_dist(e1)]);
@@ -84,6 +99,32 @@ int main(int argc, const char** argv)
 
     });
     timer.start();
+
+    int index = 0;
+    vector<std::pair<int, int>> sets =
+    {
+        {100, 100},
+        {200, 200},
+        {800, 480}
+    };
+
+    PeriodicTimer vtimer(std::chrono::seconds(10));
+    vtimer.on_timeout([&]()
+    {
+        index++;
+        if (index >= sets.size())
+            index = 0;
+        width = sets[index].first;
+        height = sets[index].second;
+
+        ostringstream ss;
+        ss << width << "," << height;
+        label_dims.set_text(ss.str());
+
+        x_dist = std::uniform_real_distribution<float>(0., static_cast<float>(win.w() - width));
+        y_dist = std::uniform_real_distribution<float>(0., static_cast<float>(win.h() - height));
+    });
+    vtimer.start();
 
     win.show();
 
