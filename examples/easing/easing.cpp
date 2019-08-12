3b073e2281f81e33bc4f81f40ea76255a18f565e
Joshua Henderson <joshua.henderson@microchip.com>
2019-08-12 11:17:28 -0700

    examples/easing: simplify implementation and cleanup
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/easing/easing.cpp
+++ examples/easing/easing.cpp
@@ -49,32 +49,11 @@ static vector<pair<easing_func_t, string>> easing_functions =
     {easing_exponential_easeinout, "exponential easeinout"}
 };
 
-static inline float_t interpolate(easing_func_t easing, float_t percent, float_t start,
-                                  float_t end, bool reverse = false)
-{
-    if (percent < 0.0f)
-        return start;
-    else if (percent > 1.0f)
-        return end;
-
-    if (reverse)
-        percent = 1.0f - easing(1.0f - percent);
-    else
-        percent = easing(percent);
-
-    return start * (1.0f - percent) + end * percent;
-}
-
 static LineChart::data_array create_data(easing_func_t easing)
 {
     LineChart::data_array data;
     for (float i = 0.; i <= 1.; i += .001)
-    {
-        LineChart::DataPair p;
-        p.x = i;
-        p.y = interpolate(easing, i, 0, 100);
-        data.push_back(p);
-    }
+        data.emplace_back(i, detail::interpolate(easing, i, 0, 100));
     return data;
 }
 
@@ -82,22 +61,11 @@ class MainWindow : public TopWindow
 {
 public:
     MainWindow()
-        : m_box(make_shared<Window>(Size(100, 200))),
-          m_seq(true),
-          m_animation(height() - 100, 0, std::chrono::seconds(2)),
-          m_delay(std::chrono::seconds(1))
+        : m_animation(height() - 100, 0, std::chrono::seconds(2))
     {
-        auto imgicon = make_shared<ImageLabel>(Image("@cursor_hand.png"));
-        imgicon->set_margin(5);
-        imgicon->resize(Size(SideBoard::HANDLE_WIDTH, SideBoard::HANDLE_WIDTH));
-        imgicon->set_align(alignmask::top | alignmask::right);
-        m_board.add(imgicon);
+        create_board();
 
-        add(m_board);
-        m_board.show();
-
-        auto i = Image("background.png");
-        auto img = make_shared<ImageLabel>(i);
+        auto img = make_shared<ImageLabel>(Image("background.png"));
         img->set_align(alignmask::expand);
         img->set_image_align(alignmask::expand);
         add(img);
@@ -110,35 +78,28 @@ public:
         list1->set_align(alignmask::expand_vertical | alignmask::right);
         add(list1);
 
-        auto line = std::make_shared<LineChart>();
-        line->set_width(m_board.width() - SideBoard::HANDLE_WIDTH);
-        line->set_color(Palette::ColorId::bg, Palette::black);
-        line->set_color(Palette::ColorId::border, Palette::white);
-        m_board.add(left(expand_vertical(line)));
-
-        list1->on_event([this, list1, line](Event&)
+        list1->on_event([this, list1](Event&)
         {
             m_seq.reset();
             m_animation.set_easing_func(easing_functions[list1->selected()].first);
             m_seq.start();
 
-            line->clear();
-            line->add_data(create_data(easing_functions[list1->selected()].first), LineChart::chart_type::lines);
+            m_line.clear();
+            m_line.add_data(create_data(easing_functions[list1->selected()].first), LineChart::chart_type::lines);
         }, {eventid::property_changed});
 
         list1->set_selected(7);
 
         auto image = std::make_shared<ImageLabel>(Image("ball.png"));
-        image->set_boxtype(Theme::boxtype::none);
+        m_box.add(image);
 
-        m_box->set_boxtype(Theme::boxtype::none);
-        m_box->add(image);
-        m_box->move(Point(width() / 2 - m_box->width() / 2, -110));
-        m_box->flags().set(Widget::flag::no_layout);
-        m_box->show();
+        m_box.set_boxtype(Theme::boxtype::none);
+        m_box.move(Point(width() / 2 - m_box.width() / 2, -110));
+        m_box.flags().set(Widget::flag::no_layout);
+        m_box.show();
         add(m_box);
 
-        m_animation.on_change(std::bind(&Window::set_y, m_box, std::placeholders::_1));
+        m_animation.on_change(std::bind(&Window::set_y, &m_box, std::placeholders::_1));
         m_seq.add(m_animation);
         m_seq.add(m_delay);
         m_seq.start();
@@ -147,11 +108,30 @@ public:
     }
 
 private:
-    shared_ptr<Window> m_box;
-    AnimationSequence m_seq;
+
+    void create_board()
+    {
+        auto imgicon = make_shared<ImageLabel>(Image("@cursor_hand.png"));
+        imgicon->set_margin(5);
+        imgicon->resize(Size(SideBoard::HANDLE_WIDTH, SideBoard::HANDLE_WIDTH));
+        imgicon->set_align(alignmask::top | alignmask::right);
+        m_board.add(imgicon);
+
+        add(m_board);
+        m_board.show();
+
+        m_line.set_width(m_board.width() - SideBoard::HANDLE_WIDTH);
+        m_line.set_color(Palette::ColorId::bg, Palette::black);
+        m_line.set_color(Palette::ColorId::border, Palette::white);
+        m_board.add(left(expand_vertical(m_line)));
+    }
+
+    Window m_box{Size(100, 200)};
+    AnimationSequence m_seq{true};
     PropertyAnimator m_animation;
-    AnimationDelay m_delay;
+    AnimationDelay m_delay{std::chrono::seconds(1)};
     SideBoard m_board;
+    LineChart m_line;
 };
 
 int main(int argc, const char** argv)
