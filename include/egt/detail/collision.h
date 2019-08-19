fd86694609424fe0b06e2433219e20d571683198
Joshua Henderson <joshua.henderson@microchip.com>
2019-08-19 16:13:37 -0700

    fix bug in alpha collision detection alogorithm
    The second variant, looking only at a single point, never worked.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- include/egt/detail/collision.h
+++ include/egt/detail/collision.h
@@ -18,12 +18,29 @@ namespace detail
 
 /**
  * Detect alpha collision between two images.
+ *
+ * This tests if any pixel from @b limage inside of @b lhs is opaque at the same
+ * time it overlaps with an opaque pixel from @b rimage in @b rhs.
+ *
+ * @param[in] lhs The origin and size of the limage surface.
+ * @param[in] limage The first image surface.
+ * @param[in] rhs The origin and size of the rimage surface.
+ * @param[in] rimage The second image surface.
  */
 bool alpha_collision(const Rect& lhs, shared_cairo_surface_t limage,
                      const Rect& rhs, shared_cairo_surface_t rimage);
 
 /**
  * Detect alpha collision with a specific point.
+ *
+ * This basically tests if the pixel at @rhs is opaque.
+ *
+ * @b lhs specified the origin with its point(). @b rhs is relative to this
+ * origin. @b lhs.size() should be the same size as the limage.
+ *
+ * @param[in] lhs The origin and size of the limage surface.
+ * @param[in] limage The image surface.
+ * @param[in] rhs The point to test.
  */
 bool alpha_collision(const Rect& lhs, shared_cairo_surface_t limage,
                      const Point& rhs);
--- src/detail/collision.cpp
+++ src/detail/collision.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "egt/detail/collision.h"
+#include <cassert>
 
 namespace egt
 {
@@ -17,19 +18,22 @@ bool alpha_collision(const Rect& lhs, shared_cairo_surface_t limage,
 {
     if (Rect::intersect(lhs, rhs))
     {
-        auto ldata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(limage.get()));
-        auto rdata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(rimage.get()));
+        assert(cairo_image_surface_get_format(limage.get()) == CAIRO_FORMAT_ARGB32);
+        assert(cairo_image_surface_get_format(rimage.get()) == CAIRO_FORMAT_ARGB32);
 
-        Rect i = Rect::intersection(lhs, rhs);
+        const auto ldata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(limage.get()));
+        const auto rdata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(rimage.get()));
+        const auto pitch = cairo_image_surface_get_stride(limage.get()) / sizeof(uint32_t);
+        const auto i = Rect::intersection(lhs, rhs);
 
-        for (int y = i.top(); y < i.bottom(); y++)
+        for (auto y = i.top(); y < i.bottom(); y++)
         {
-            for (int x = i.left(); x < i.right(); x++)
+            for (auto x = i.left(); x < i.right(); x++)
             {
-                unsigned int l = ldata[static_cast<uint32_t>((x - lhs.left()) + (y - lhs.top()) * lhs.width())];
-                unsigned int r = rdata[static_cast<uint32_t>((x - rhs.left()) + (y - rhs.top()) * rhs.width())];
+                auto l = ldata[static_cast<uint32_t>((x - lhs.left()) + (y - lhs.top()) * pitch)];
+                auto r = rdata[static_cast<uint32_t>((x - rhs.left()) + (y - rhs.top()) * pitch)];
 
-                if ((l >> 24 & 0xff) && (r >> 24 & 0xff))
+                if (((l >> 24) & 0xff) && ((r >> 24) & 0xff))
                 {
                     return true;
                 }
@@ -45,19 +49,16 @@ bool alpha_collision(const Rect& lhs, shared_cairo_surface_t limage,
 {
     if (Rect::intersect(lhs, rhs))
     {
-        auto ldata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(limage.get()));
+        assert(cairo_image_surface_get_format(limage.get()) == CAIRO_FORMAT_ARGB32);
 
-        for (int y = lhs.top(); y < lhs.bottom(); y++)
+        const auto ldata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(limage.get()));
+        const auto pitch = cairo_image_surface_get_stride(limage.get()) / sizeof(uint32_t);
+
+        const auto l = ldata[static_cast<uint32_t>((rhs.x() - lhs.left()) + (rhs.y() - lhs.top()) * pitch)];
+
+        if ((l >> 24) & 0xff)
         {
-            for (int x = lhs.left(); x < lhs.right(); x++)
-            {
-                uint32_t l = ldata[static_cast<uint32_t>((x - lhs.left()) + (y - lhs.top()) * lhs.width())];
-
-                if (l >> 24 & 0xff)
-                {
-                    return true;
-                }
-            }
+            return true;
         }
     }
 
