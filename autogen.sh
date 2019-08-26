d4b3762fce8d44b79a0103e169008372beff35fc
Joshua Henderson <joshua.henderson@microchip.com>
2019-08-26 09:23:02 -0700

    autogen.sh: automatically update submodules
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+modules_initialized=0
+SUBMODULES=$(cat .gitmodules | grep 'path.*=' | cut -d'=' -f2)
+for submod in $SUBMODULES; do
+    if [ -e $submod/.git ]; then
+	modules_initialized=1
+    fi
+done
+
+# don't update if there are any submodule directories
+if [ -f .gitmodules ] && [ -d .git ] && [ $modules_initialized = 0 ]; then
+    git submodule update --init --recursive
+fi
+
 autoreconf --force --verbose --install || exit 1
