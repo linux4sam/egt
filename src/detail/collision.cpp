cf1f714ecbf9292fce56284dc7c5cb20ad1be268
Joshua Henderson <joshua.henderson@microchip.com>
2019-09-04 16:40:58 -0700

    fix alpha_collision() when surface are different sizes
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- src/detail/collision.cpp
+++ src/detail/collision.cpp
@@ -23,15 +23,16 @@ bool alpha_collision(const Rect& lhs, shared_cairo_surface_t limage,
 
         const auto ldata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(limage.get()));
         const auto rdata = reinterpret_cast<unsigned int*>(cairo_image_surface_get_data(rimage.get()));
-        const auto pitch = cairo_image_surface_get_stride(limage.get()) / sizeof(uint32_t);
+        const auto lpitch = cairo_image_surface_get_stride(limage.get()) / sizeof(uint32_t);
+        const auto rpitch = cairo_image_surface_get_stride(rimage.get()) / sizeof(uint32_t);
         const auto i = Rect::intersection(lhs, rhs);
 
         for (auto y = i.top(); y < i.bottom(); y++)
         {
             for (auto x = i.left(); x < i.right(); x++)
             {
-                auto l = ldata[static_cast<uint32_t>((x - lhs.left()) + (y - lhs.top()) * pitch)];
-                auto r = rdata[static_cast<uint32_t>((x - rhs.left()) + (y - rhs.top()) * pitch)];
+                auto l = ldata[static_cast<uint32_t>((x - lhs.left()) + (y - lhs.top()) * lpitch)];
+                auto r = rdata[static_cast<uint32_t>((x - rhs.left()) + (y - rhs.top()) * rpitch)];
 
                 if (((l >> 24) & 0xff) && ((r >> 24) & 0xff))
                 {
