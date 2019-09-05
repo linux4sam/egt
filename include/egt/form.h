77e3f9489f7c31ca0a3ef43bf55d8b5ccc6bf0f1
Joshua Henderson <joshua.henderson@microchip.com>
2019-09-05 11:21:54 -0700

    move Form implementation to form.cpp
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- include/egt/form.h
+++ include/egt/form.h
@@ -12,8 +12,6 @@
  */
 
 #include <egt/frame.h>
-#include <egt/grid.h>
-#include <egt/label.h>
 #include <egt/sizer.h>
 #include <memory>
 #include <string>
@@ -34,44 +32,11 @@ class Form : public Frame
 {
 public:
 
-    explicit Form(const std::string& title = {}) noexcept
-        : m_vsizer(orientation::vertical)
-    {
-        set_name("Form" + std::to_string(m_widgetid));
+    explicit Form(const std::string& title = {}) noexcept;
 
-        m_vsizer.set_align(alignmask::expand);
-        add(m_vsizer);
+    explicit Form(Frame& parent, const std::string& title = {}) noexcept;
 
-        if (!title.empty())
-        {
-            auto label = std::make_shared<Label>(title, alignmask::center);
-            label->set_font(Font(label->font().face(),
-                                 label->font().size() + 4,
-                                 Font::weightid::bold,
-                                 label->font().slant()));
-            label->set_align(alignmask::expand_horizontal);
-            m_vsizer.add(label);
-        }
-    }
-
-    explicit Form(Frame& parent, const std::string& title = {}) noexcept
-        : Form(title)
-    {
-        parent.add(*this);
-    }
-
-    virtual void add_group(const std::string& caption)
-    {
-        auto label = std::make_shared<Label>(caption);
-        label->set_font(Font(label->font().face(),
-                             label->font().size(),
-                             Font::weightid::bold,
-                             label->font().slant()));
-        label->set_align(alignmask::expand_horizontal);
-        label->set_text_align(alignmask::bottom | alignmask::left);
-        m_vsizer.add(label);
-        m_options.push_back(label.get());
-    }
+    virtual void add_group(const std::string& caption);
 
     template<class T>
     void add_option(const std::string& name, const std::shared_ptr<T>& widget)
@@ -80,39 +45,13 @@ public:
         add_option(name, p);
     }
 
-    virtual void add_option(const std::string& name, const std::shared_ptr<Widget>& widget)
-    {
-        widget->set_align(alignmask::expand);
-        auto label = std::make_shared<Label>(name);
-        label->set_align(alignmask::expand);
-        label->set_text_align(alignmask::center | alignmask::left);
-        auto grid = std::make_shared<StaticGrid>(Tuple(2, 1));
-        grid->set_margin(2); /// @todo Not working
-        grid->resize(Size(0, widget->min_size_hint().height()));
-        grid->set_align(alignmask::expand_horizontal);
-        grid->add(label);
-        grid->add(widget);
-        m_vsizer.add(grid);
-        m_options.push_back(grid.get());
-    }
+    virtual void add_option(const std::string& name, const std::shared_ptr<Widget>& widget);
 
-    virtual void add_option(const std::shared_ptr<Widget>& widget)
-    {
-        widget->set_align(alignmask::expand);
-        auto grid = std::make_shared<StaticGrid>(Tuple(1, 1));
-        grid->set_margin(2); /// @todo Not working
-        grid->resize(Size(0, widget->min_size_hint().height()));
-        grid->set_align(alignmask::expand_horizontal);
-        grid->add(widget);
-        m_vsizer.add(grid);
-        m_options.push_back(grid.get());
-    }
+    virtual void add_option(const std::shared_ptr<Widget>& widget);
 
-    virtual void remove_options()
-    {
-        for (auto option : m_options)
-            m_vsizer.remove(option);
-    }
+    virtual void remove_options();
+
+    virtual ~Form() = default;
 
 protected:
 
---
+++ src/form.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "egt/form.h"
+#include "egt/grid.h"
+#include "egt/label.h"
+
+using namespace std;
+
+namespace egt
+{
+inline namespace v1
+{
+namespace experimental
+{
+
+Form::Form(const std::string& title) noexcept
+    : m_vsizer(orientation::vertical)
+{
+    set_name("Form" + std::to_string(m_widgetid));
+
+    m_vsizer.set_align(alignmask::expand);
+    add(m_vsizer);
+
+    if (!title.empty())
+    {
+        auto label = std::make_shared<Label>(title, alignmask::center);
+        label->set_font(Font(label->font().face(),
+                             label->font().size() + 4,
+                             Font::weightid::bold,
+                             label->font().slant()));
+        label->set_align(alignmask::expand_horizontal);
+        m_vsizer.add(label);
+    }
+}
+
+Form::Form(Frame& parent, const std::string& title) noexcept
+    : Form(title)
+{
+    parent.add(*this);
+}
+
+void Form::add_group(const std::string& caption)
+{
+    auto label = std::make_shared<Label>(caption);
+    label->set_font(Font(label->font().face(),
+                         label->font().size(),
+                         Font::weightid::bold,
+                         label->font().slant()));
+    label->set_align(alignmask::expand_horizontal);
+    label->set_text_align(alignmask::bottom | alignmask::left);
+    m_vsizer.add(label);
+    m_options.push_back(label.get());
+}
+
+void Form::add_option(const std::string& name, const std::shared_ptr<Widget>& widget)
+{
+    widget->set_align(alignmask::expand);
+    auto label = std::make_shared<Label>(name);
+    label->set_align(alignmask::expand);
+    label->set_text_align(alignmask::center | alignmask::left);
+    auto grid = std::make_shared<StaticGrid>(Tuple(2, 1));
+    grid->set_margin(2); /// @todo Not working
+    grid->resize(Size(0, widget->min_size_hint().height()));
+    grid->set_align(alignmask::expand_horizontal);
+    grid->add(label);
+    grid->add(widget);
+    m_vsizer.add(grid);
+    m_options.push_back(grid.get());
+}
+
+void Form::add_option(const std::shared_ptr<Widget>& widget)
+{
+    widget->set_align(alignmask::expand);
+    auto grid = std::make_shared<StaticGrid>(Tuple(1, 1));
+    grid->set_margin(2); /// @todo Not working
+    grid->resize(Size(0, widget->min_size_hint().height()));
+    grid->set_align(alignmask::expand_horizontal);
+    grid->add(widget);
+    m_vsizer.add(grid);
+    m_options.push_back(grid.get());
+}
+
+void Form::remove_options()
+{
+    for (auto option : m_options)
+        m_vsizer.remove(option);
+}
+
+}
+}
+}
