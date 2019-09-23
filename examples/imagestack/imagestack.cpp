72c23e24f80631c16a0424ce7a122052013579c0
Joshua Henderson <joshua.henderson@microchip.com>
2019-09-23 15:18:02 -0700

    examples/imagestack: new example
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ examples/imagestack/imagestack.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <egt/detail/imagecache.h>
+#include <egt/ui>
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace egt;
+
+class MainWindow : public TopWindow
+{
+public:
+
+    class LauncherItem : public Frame
+    {
+    public:
+        LauncherItem(const Image& image)
+            : m_background(image)
+        {
+            set_boxtype(Theme::boxtype::none);
+            flags().set(Widget::flag::no_layout);
+
+            m_background.set_align(alignmask::expand);
+            m_background.set_image_align(alignmask::expand);
+            add(m_background);
+        }
+
+    private:
+        ImageLabel m_background;
+    };
+
+    MainWindow()
+    {
+        set_background(Image("background.png"));
+
+        auto egt_logo = std::make_shared<ImageLabel>(Image("@128px/egt_logo_black.png"));
+        egt_logo->set_align(alignmask::center | alignmask::top);
+        egt_logo->set_margin(5);
+        add(egt_logo);
+
+        m_animation.set_starting(0);
+        m_animation.set_duration(std::chrono::seconds(2));
+        m_animation.set_easing_func(easing_cubic_easeout);
+        m_animation.on_change(std::bind(&MainWindow::move_boxes,
+                                        this, std::placeholders::_1));
+        m_seq.add(m_delay);
+        m_seq.add(m_animation);
+
+        load();
+    }
+
+    void handle(Event& event) override
+    {
+        TopWindow::handle(event);
+
+        switch (event.id())
+        {
+        case eventid::pointer_drag_start:
+            m_seq.reset();
+            if (!m_boxes.empty())
+                m_start = m_boxes.front()->x();
+            break;
+        case eventid::pointer_drag:
+            move_boxes(event.pointer().point.x() -
+                       event.pointer().drag_start.x());
+            event.stop();
+            break;
+        case eventid::pointer_drag_stop:
+            if (!m_boxes.empty())
+            {
+                m_start = m_boxes.front()->x();
+                m_animation.set_ending(width() - m_boxes.front()->width() - m_boxes.front()->x());
+                m_seq.start();
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    void move_boxes(int diff = 0)
+    {
+        if (m_boxes.empty())
+            return;
+
+        const auto& last = m_boxes.back();
+        if (diff < 0 && last->x() <= center().x())
+            return;
+
+        auto x = m_start + diff;
+        for (auto& box : m_boxes)
+        {
+            if (x + box->width() > width())
+                x = width() - box->width();
+            box->set_x(x);
+            x += box->width() + 10;
+        }
+    }
+
+private:
+
+    void load()
+    {
+        for (auto x = 0; x < 9; x++)
+        {
+            auto image = Image("image" + std::to_string(x) + ".png");
+            auto box = std::make_shared<LauncherItem>(image);
+            box->resize(Size(width() / 4, height() - 100));
+            m_boxes.push_back(box);
+        }
+
+        for (auto& box : detail::reverse_iterate(m_boxes))
+        {
+            box->set_y(center().y() - box->height() / 2);
+            box->set_x(width() - box->width());
+            add(box);
+        }
+
+        if (!m_boxes.empty())
+        {
+            m_start = m_boxes.front()->x();
+            move_boxes();
+        }
+    }
+
+    std::vector<std::shared_ptr<LauncherItem>> m_boxes;
+    int m_start{0};
+    AnimationSequence m_seq;
+    AnimationDelay m_delay{std::chrono::seconds(2)};
+    PropertyAnimator m_animation;
+};
+
+int main(int argc, const char** argv)
+{
+    Application app(argc, argv, "imagestack");
+
+    detail::add_search_path("images/");
+
+    MainWindow window;
+    window.show();
+
+    return app.run();
+}
