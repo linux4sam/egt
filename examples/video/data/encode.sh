7ded1864c4eb6961202423c0a5ce9e3d6997702b
Joshua Henderson <joshua.henderson@microchip.com>
2019-10-15 09:44:53 -0700

    examples/video: optimize mpeg video encodes and simplify script
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/video/data/encode.sh
+++ examples/video/data/encode.sh
@@ -63,7 +63,7 @@ fi
 
 set -e
 
-audio_opts="-acodec pcm_s16le -ac 2 -ar 48000"
+audio_opts="-acodec pcm_s16le -ac 1 -ar 48000"
 size_opts="-vf scale=(iw*sar)*max(${width}/(iw*sar)\,${height}/ih):\
 ih*max(${width}/(iw*sar)\,${height}/ih),crop=${width}:${height} \
 -s ${width}x${height}"
@@ -72,66 +72,57 @@ mpeg_opts="-pre_dia_size 5 -dia_size 5 -qcomp 0.7 -qblur 0 -preme 2 \
 -me_method dia -sc_threshold 0 -sc_factor 4 -bidir_refine 4 -profile:v 4 \
 -mbd rd -mbcmp satd -precmp satd -cmp satd -subcmp satd -skipcmp satd -bf 2"
 
+base="${in%.*}"
+
 for t in $outputs
 do
     case "$t" in
 	mpeg1)
-	    ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
-		   -vcodec mpeg1video -q:v 1 -b_strategy 2 -brd_scale 2 \
-		   ${mpeg_opts} -pass 1 -f avi /dev/null
+	    cmd=(ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
+		 -vcodec mpeg1video -qscale:v 2 -b_strategy 2 -brd_scale 2 \
+		 ${mpeg_opts} -threads 4)
 
-	    ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
-		   -vcodec mpeg1video -q:v 1 ${mpeg_opts} -pass 2 \
-		   "${in}".mpeg1.avi
+	    "${cmd[@]}" -pass 1 -f avi /dev/null
+	    "${cmd[@]}" -pass 2 "${base}_mpeg1.avi"
 	    ;;
 	mpeg2)
-#	    ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
-#		   -vcodec mpeg2video -q:v 1 -b_strategy 2 -brd_scale 2 \
-#		   ${mpeg_opts} -pass 1 -f avi /dev/null
+	    cmd=(ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
+		 -vcodec mpeg2video -qscale:v 2 -b_strategy 2 -brd_scale 2 \
+		 ${mpeg_opts} -threads 4)
 
-#	    ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
-#		   -vcodec mpeg2video -q:v 1 ${mpeg_opts} -pass 2 "${in}".mpeg2.avi
-
-    	    ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
-		   -vcodec mpeg2video -q:v 1 ${mpeg_opts} "${in}".mpeg2.avi
+	    "${cmd[@]}" -pass 1 -f avi /dev/null
+	    "${cmd[@]}" -pass 2 "${base}_mpeg2.avi"
 	    ;;
 	mpeg4)
-	    #ffmpeg -y -i "${in}" ${size_opts} \
-	#	   ${audio_opts} ${video_opts} -vcodec mpeg4 -q:v 2 -pass 1 \
-	#	   -f avi /dev/null
+	    cmd=(ffmpeg -y -i "${in}" ${size_opts} ${audio_opts} ${video_opts} \
+		 -vcodec mpeg4 -qscale:v 2 ${mpeg_opts} -threads 4)
 
-	 #   ffmpeg -y -i "${in}" ${size_opts} \
-	#	   ${audio_opts} ${video_opts} -vcodec mpeg4 -q:v 2 -pass 2 \
-	#	   "${in}".mpeg4.avi
-
-
-	    ffmpeg -y -i "${in}" ${size_opts} \
-		   ${audio_opts} ${video_opts} -vcodec mpeg4 -q:v 2 \
-		   "${in}".mpeg4.avi
+	    "${cmd[@]}" -pass 1 -f avi /dev/null
+	    "${cmd[@]}" -pass 2 "${base}_mpeg4.avi"
 	    ;;
 	vp8)
 	    ffmpeg -y -i "${in}" ${size_opts} \
 		   ${audio_opts} ${video_opts} -vcodec libvpx -crf 5 -qmin 0 \
-		   -qmax 50 -q:v 3 "${in}".vp8.avi
+		   -qmax 50 -q:v 3 "${base}_vp8.avi"
 	    ;;
 	vp9)
 	    ffmpeg -y -i "${in}" ${size_opts} \
 		   ${audio_opts} ${video_opts} -vcodec libvpx-vp9 -crf 5 -b:v 0 \
-		   -qmin 0 -qmax 50 -q:v 3 "${in}".vp9.avi
+		   -qmin 0 -qmax 50 -q:v 3 "${base}_vp9.avi"
 	    ;;
 	h264)
 	    ffmpeg -y -i "${in}" ${size_opts} \
 		   ${audio_opts} -strict experimental ${video_opts} -vcodec h264 \
-		   -profile:v baseline -q:v 0 "${in}".h264.avi
+		   -profile:v baseline -q:v 0 "${base}_h264.avi"
 	    ;;
 	uncompressed)
 	    ffmpeg -y -i "${in}" ${size_opts} \
 		   -c:v rawvideo ${video_opts} -q:v 2 ${audio_opts} \
-		   "${in}".uncompressed.avi
+		   "${base}_uncompressed.avi"
 	    ;;
 	raw)
 	    ffmpeg -y -i "${in}" ${size_opts} \
-		   -c:v rawvideo ${video_opts} -q:v 2 "${in}".yuv
+		   -c:v rawvideo ${video_opts} -q:v 2 "${base}.yuv"
 	    ;;
 	*)
 	    echo "type $type not supported."
