4734554ae6623172efbd770954639cd9031270da
Joshua Henderson <joshua.henderson@microchip.com>
2020-02-28 13:57:39 -0700

    examples/video: on host builds, force transparent control window
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/video/video.cpp
+++ examples/video/video.cpp
@@ -84,9 +84,11 @@ int main(int argc, char** argv)
     player.volume(5);
     win.add(player);
 
-    egt::Window ctrlwindow(egt::Size(win.width(), 72));
+    egt::Window ctrlwindow(egt::Size(win.width(), 72), egt::PixelFormat::argb8888);
     ctrlwindow.align(egt::AlignFlag::bottom | egt::AlignFlag::center);
     ctrlwindow.color(egt::Palette::ColorId::bg, egt::Palette::transparent);
+    if (!ctrlwindow.flags().is_set(egt::Window::Flag::plane_window))
+        ctrlwindow.fill_flags(egt::Theme::FillFlag::blend);
     win.add(ctrlwindow);
 
     egt::HorizontalBoxSizer hpos;
