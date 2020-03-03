b1a37cbba1c1cd03b3c30b6099e369fea22e9726
Joshua Henderson <joshua.henderson@microchip.com>
2020-03-03 08:33:48 -0700

    layout: reorder members of layout struct big to small
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- include/egt/detail/layout.h
+++ include/egt/detail/layout.h
@@ -25,16 +25,14 @@ namespace detail
  */
 struct EGT_API LayoutRect
 {
-    LayoutRect() = default;
-
     LayoutRect(uint32_t b,
                const Rect& r,
                uint32_t lm = 0,
                uint32_t tm = 0,
                uint32_t rm = 0,
                uint32_t bm = 0) noexcept
-        : behave(b),
-          rect(r),
+        : rect(r),
+          behave(b),
           lmargin(lm),
           tmargin(tm),
           rmargin(rm),
@@ -48,22 +46,22 @@ struct EGT_API LayoutRect
                uint32_t tm = 0,
                uint32_t rm = 0,
                uint32_t bm = 0) noexcept
-        : behave(b),
+        : str(std::move(s)),
           rect(r),
+          behave(b),
           lmargin(lm),
           tmargin(tm),
           rmargin(rm),
-          bmargin(bm),
-          str(std::move(s))
+          bmargin(bm)
     {}
 
-    uint32_t behave{0};
+    std::string str;
     Rect rect;
+    uint32_t behave{0};
     uint32_t lmargin{0};
     uint32_t tmargin{0};
     uint32_t rmargin{0};
     uint32_t bmargin{0};
-    std::string str;
 };
 
 /**
