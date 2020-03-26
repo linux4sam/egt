c67b4abd3c095cb05e55c2a96998d1eb46a74ea3
Sandeep Sheriker M <sandeep.sheriker@microchip.com>
2020-03-26 13:26:11 -0700

    test: add unit test for grid widget
    Signed-off-by: Sandeep Sheriker M <sandeep.sheriker@microchip.com>

---
+++ test/widgets/grid.cpp
@@ -0,0 +1,153 @@
+
+#include <egt/ui>
+#include <gtest/gtest.h>
+#include <random>
+
+using namespace egt;
+
+using ::testing::Combine;
+using ::testing::TestWithParam;
+using ::testing::Values;
+using ::testing::Range;
+
+static int random_item(int start, int end)
+{
+    std::random_device r;
+    std::default_random_engine e {r()};
+    std::uniform_int_distribution<int> dist(start, end);
+    return dist(e);
+}
+
+class StaticGridTest : public testing::TestWithParam<::testing::tuple<int, int>> {};
+
+TEST_P(StaticGridTest, TestWidget)
+{
+    egt::Application app;
+    egt::TopWindow win;
+    std::shared_ptr<egt::StaticGrid> widget;
+
+    auto rows = ::testing::get<0>(GetParam());
+    auto columns = ::testing::get<1>(GetParam());
+
+    std::cout << "Grid size = " << rows << " x " << columns << std::endl;
+
+    widget.reset(new StaticGrid(std::make_tuple(columns, rows), 5));
+    win.add(egt::expand(widget));
+
+    EXPECT_TRUE(widget->last_add_column() == -1);
+    EXPECT_TRUE(widget->last_add_row() == -1);
+
+    PeriodicTimer cputimer(std::chrono::seconds(2));
+    if ((rows == 0) || (columns == 0))
+    {
+        ASSERT_EXIT((widget->get(Point(0, 0)), exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+
+        auto text1 = std::make_shared<egt::TextBox>("text 1", Rect(0, 0, 200, 100));
+        ASSERT_EXIT((widget->add(egt::expand(text1)), exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+    }
+    else
+    {
+        EXPECT_TRUE(widget->get(Point(0, 0)) == nullptr);
+
+        for (int i = 0; i <  rows;  i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                auto text1 = std::make_shared<egt::TextBox>("text " + std::to_string(j) + " " + std::to_string(i));
+                widget->add(egt::expand(text1));
+                EXPECT_EQ(widget->last_add_row(), i);
+                EXPECT_EQ(widget->last_add_column(), j);
+            }
+        }
+        EXPECT_TRUE(static_cast<int>(widget->count_children()) == (rows * columns));
+
+        cputimer.on_timeout([widget, &cputimer, &app]()
+        {
+            int r = widget->last_add_row();
+            int c = widget->last_add_column();
+            auto rm = widget->get(Point(c, r));
+            if (rm != nullptr)
+            {
+                widget->remove(rm);
+            }
+            else
+            {
+                cputimer.stop();
+                app.quit();
+            }
+        });
+        cputimer.start();
+    }
+
+    win.show();
+    app.run();
+}
+
+INSTANTIATE_TEST_SUITE_P(StaticGridTestGroup, StaticGridTest, Combine(Range(1, 3), Range(1, 3)));
+
+
+class SelectableGridTest : public testing::TestWithParam<::testing::tuple<int, int>> {};
+
+TEST_P(SelectableGridTest, TestWidget)
+{
+    egt::Application app;
+    egt::TopWindow win;
+    std::shared_ptr<egt::SelectableGrid> widget;
+
+    auto rows = ::testing::get<0>(GetParam());
+    auto columns = ::testing::get<1>(GetParam());
+
+    std::cout << "Grid size = " << rows << " x " << columns << std::endl;
+
+    widget.reset(new SelectableGrid(std::make_tuple(columns, rows), 5));
+    win.add(egt::expand(widget));
+
+    EXPECT_TRUE(widget->last_add_column() == -1);
+    EXPECT_TRUE(widget->last_add_row() == -1);
+
+    PeriodicTimer cputimer(std::chrono::seconds(2));
+    if ((rows == 0) || (columns == 0))
+    {
+        ASSERT_EXIT((widget->get(Point(0, 0)), exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+
+        auto text1 = std::make_shared<egt::TextBox>("text 1", Rect(0, 0, 200, 100));
+        ASSERT_EXIT((widget->add(egt::expand(text1)), exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+    }
+    else
+    {
+        EXPECT_TRUE(widget->get(Point(0, 0)) == nullptr);
+
+        for (int i = 0; i <  rows;  i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                auto text1 = std::make_shared<egt::Button>("Button " + std::to_string(j) + " " + std::to_string(i));
+                widget->add(egt::expand(text1));
+                EXPECT_EQ(widget->last_add_row(), i);
+                EXPECT_EQ(widget->last_add_column(), j);
+            }
+        }
+        EXPECT_TRUE(static_cast<int>(widget->count_children()) == (rows * columns));
+
+        cputimer.on_timeout([widget, &cputimer, &app]()
+        {
+            static int count = 8;
+            int r = random_item(0, widget->last_add_row());
+            int c = random_item(0, widget->last_add_column());
+            std::cout << "Selecting Widget = " << c << " x " << r << std::endl;
+            widget->selected(c, r);
+            if (++count >= 8)
+            {
+                count = 0;
+                cputimer.stop();
+                app.quit();
+            }
+        });
+        cputimer.start();
+    }
+
+    win.show();
+    app.run();
+}
+
+INSTANTIATE_TEST_SUITE_P(SelectableGridTestGroup, SelectableGridTest, Combine(Range(1, 5), Range(1, 5)));
