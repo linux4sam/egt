6111a860542eb91946b6b921326efe0c5fea954b
Joshua Henderson <joshua.henderson@microchip.com>
2020-04-03 12:08:18 -0700

    docs: support spaces in paths for drop shadow script
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- docs/src/dropshadow.sh
+++ docs/src/dropshadow.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-convert $1 \( +clone -background black -shadow 80x3+2+2 \) +swap -background transparent -layers merge +repage $1
+convert "$1" \( +clone -background black -shadow 80x3+2+2 \) +swap -background transparent -layers merge +repage "$1"
