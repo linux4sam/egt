085beec484ec9a88a75061c4af3235ed1e8c1bf5
Joshua Henderson <joshua.henderson@microchip.com>
2020-05-18 15:02:11 -0700

    examples/basicui: add command line argument for file to load
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- examples/basicui/basicui.cpp
+++ examples/basicui/basicui.cpp
@@ -4,8 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 /// @[Example]
+#include <cxxopts.hpp>
 #include <egt/ui>
 #include <egt/uiloader.h>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
@@ -14,8 +16,19 @@ int main(int argc, char** argv)
     egt::add_search_path(EXAMPLEDATA);
 #endif
 
+    cxxopts::Options options(argv[0], "load ui xml");
+    options.add_options()
+    ("h,help", "Show help")
+    ("i,input", "Input file", cxxopts::value<std::string>()->default_value("file:ui.xml"));
+    auto args = options.parse(argc, argv);
+    if (args.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
     egt::experimental::UiLoader loader;
-    auto window = loader.load("file:ui.xml");
+    auto window = loader.load(args["input"].as<std::string>());
     window->show();
 
     return app.run();
