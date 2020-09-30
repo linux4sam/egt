2993b1baf6f07d00f429921883e95f2d987baf50
Nicolas Ferre <nicolas.ferre@microchip.com>
2020-09-30 19:09:14 +0200

    svgimage: add documentation for the elements of a SVG image
    Make it clear how to use the individual elements of a SVG file.
    Syntax matching with librsvg needs and addition of examples.
    Id Parameter of methods also documented.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@microchip.com>

--- include/egt/svgimage.h
+++ include/egt/svgimage.h
@@ -34,6 +34,21 @@ inline namespace v1
  * SvgImage svg("file.svg");
  * @endcode
  *
+ * For using individual elements from a SVG file and render them separately
+ * the document must be created with element IDs. In the dashboard example for
+ * instance, one of the \p dash_background.svg element has the attribute:
+ * @code{.svg}
+ * id="right_blink"
+ * @endcode
+ * Several methods in the SvgImage class allow the use of such elements when
+ * the \p id parameter is present. Element IDs must follow the syntax of an URL
+ * fragment identifier. For example, use "#right_blink" (hash right_blink) to
+ * obtain a handle on the SVG element mentioned just above; which gives:
+ * @code{.cpp}
+ * SvgImage custom1svg("file:dash_background.svg", SizeF(200, 0));
+ * auto img_box = custom1svg.id_box("#right_blink");
+ * @endcode
+ *
  * @note Some standard effects in SVG files are expensive to render. For
  * example, some patterns and gradients.  In those cases, you can convert
  * elements in the SVG itself to raster images to speed up rendering here.
@@ -75,7 +90,11 @@ public:
      * Image returned will be the same size as the entire SVG.
      *
      * @param[in] id Optional id of the SVG element to render.  If no id is
-     *               specified, all elements in the SVG will be rendered.
+     *               specified, all elements in the SVG will be rendered. \n 
+     *               Element IDs must follow the syntax of an URL fragment
+     *               identifier.  For example, use "#right_blink" (hash right_blink)
+     *               to obtain a handle on the SVG element with
+     *               id="right_blink".
      * @param[in] rect Optional rect to clip to.
      */
     EGT_NODISCARD Image render(const std::string& id = {}, const RectF& rect = {}) const;
@@ -101,14 +120,22 @@ public:
     /**
      * Get the position and size of an element in the SVG.
      *
-     * @param[in] id The id of the SVG element.
+     * @param[in] id The id of the SVG element. \n 
+     *               Element IDs must follow the syntax of an URL fragment
+     *               identifier.  For example, use "#right_blink" (hash right_blink)
+     *               to obtain a handle on the SVG element with
+     *               id="right_blink".
      */
     EGT_NODISCARD RectF id_box(const std::string& id) const;
 
     /**
      * Test if an element exists in the SVG image.
      *
-     * @param[in] id The id of the SVG element.
+     * @param[in] id The id of the SVG element. \n 
+     *               Element IDs must follow the syntax of an URL fragment
+     *               identifier.  For example, use "#right_blink" (hash right_blink)
+     *               to obtain a handle on the SVG element with
+     *               id="right_blink".
      * @return true if found
      */
     EGT_NODISCARD bool id_exists(const std::string& id) const;
