117adc083583cc09fffeb192cd5f6907c1feda22
Ludovic Desroches <ludovic.desroches@microchip.com>
2020-10-14 08:30:21 +0200

    Revert "video: add on_click signal"
    This reverts commit 6b0843903b1ac5e26bbbcabf31ee47672cd2fd98.

--- include/egt/video.h
+++ include/egt/video.h
@@ -63,11 +63,6 @@ public:
 
     /// Invoked when the state of the player changes.
     Signal<> on_state_changed;
-
-    /**
-     * Invoked on clicked event
-     */
-    Signal<> on_click;
     /** @} */
 
     /**
