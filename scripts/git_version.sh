7605c52cb12cbd7a0ff5da59eb4c70a54c56eaeb
Ludovic Desroches <ludovic.desroches@microchip.com>
2021-06-28 16:19:18 +0200

    git_version: misc fixes
    Don't exit with an error, it causes Makefile to fail.
    Prefer a string with no space as they have to be escaped. Otherwise
    -DGIT_VERSION="..." will cause an error.
    Fix top_builddir vs top_srcdir in the automake file, build in another
    directory than the root was failing.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>

--- scripts/git_version.sh
+++ scripts/git_version.sh
@@ -4,18 +4,18 @@
 # Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
 # All rights reserved.
 #
-# Script to enforce the coding style of this project.
+# Script to get, if applicable, the current state of the git tree.
 #
 
 if ! [ -x "$(command -v git)" ] ; then
-    echo "not available"
-    exit 1
+    echo "n/a"
+    exit 0
 fi
 
 GIT_COMMIT=$(git log -1 --pretty='%h' 2> /dev/null)
 if [ $? -ne 0 ] ; then
-    echo "not available"
-    exit 1
+    echo "n/a"
+    exit 0
 fi
 
 git diff --quiet
