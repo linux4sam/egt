70721aea93c40c9fe0120a134c689fae7cdeeed8
Ludovic Desroches <ludovic.desroches@microchip.com>
2022-03-01 09:05:26 +0100

    ComboBox: new API for add_item
    Add a new API for add_item in order to not delegate the ownership of the
    StringItem. It can be useful, in some cases, to prevent the destuction of
    the item when it's removed from the list.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    Acked-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>

--- include/egt/combo.h
+++ include/egt/combo.h
@@ -136,6 +136,24 @@ public:
     void add_item(const std::shared_ptr<StringItem>& item);
 
     /**
+     * Append a new item to the ComboBox.
+     *
+     * @param item The item.
+     *
+     * @warning This does not manage the lifetime of StringItem. It is up to
+     * the caller to make sure this StringItem is available for as long as the
+     * instance of this class is around.
+     */
+    void add_item(StringItem& item)
+    {
+        // Nasty, but it gets the job done.  If a widget is passed in as a
+        // reference, we don't own it, so create a "pointless" shared_ptr that
+        // will not delete it.
+        auto i = std::shared_ptr<StringItem>(&item, [](StringItem*) {});
+        add_item(i);
+    }
+
+    /**
      * Remove an item from a ComboBox.
      */
     void remove_item(StringItem* item);
