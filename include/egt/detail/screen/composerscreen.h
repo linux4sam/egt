86a999e3c11506cfe0c18646cbfa6e71bf167efc
Cyrille Pitchen <cyrille.pitchen@microchip.com>
2022-04-28 13:56:30 +0200

    ComposerScreen: notify watchers about screen size changes with a signal
    Add a 'on_screen_resized' signal as a static member of the
    ComposerScreen and use it to notify watchers when the screen size is
    changed from the MGC. This signal will be used for instance to recompute
    the default sizes of fonts and widgets.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>

--- include/egt/detail/screen/composerscreen.h
+++ include/egt/detail/screen/composerscreen.h
@@ -12,6 +12,7 @@
  */
 
 #include <egt/screen.h>
+#include <egt/signal.h>
 
 namespace egt
 {
@@ -36,6 +37,47 @@ public:
     EGT_NODISCARD unsigned char* get_pixmap();
 
     void resize(const Size& size);
+
+    /**
+     * Register a handler to manage screen size changes.
+     *
+     * @param[in]: handler a callback to execute when the 'on_screen_resized'
+     *             signal is invoked.
+     *
+     * @return a handle that can be unregister later.
+     *
+     * @note This signal-base mechanism is suited to manage 'static' watchers
+     * like default widget sizes, especially within templates, because the
+     * ComposerScreen itself can't guess exhaustive lists of all templates
+     * instances (SliderType<int>, SliderType<float>, ...) to reset the default
+     * sizes for all of them.
+     *
+     * However, it is not suited to manage 'instance' watchers like fonts and
+     * widgets for which we should have to deal with registrations from their
+     * copy/move constructors and assignment operators and with unregistrations
+     * from the destructors.
+     *
+     * Besides, this method is 'static' so it can be called before the
+     * ComposerScreen instance is created (from MGC plugin) or even if it is
+     * not constructed at all, which is what happens anywhere else, like on
+     * targets.
+     */
+    static Signal<>::RegisterHandle register_screen_resize_hook(const Signal<>::EventCallback& handler);
+
+    /**
+     * Unregister a handler that managed screen size changes.
+     *
+     * @param[in]: handle the returned value of register_screen_resize_hook()
+     *
+     * @note This method is 'static' like unregister_screen_resize_hook() for
+     * the very same reason.
+     *
+     */
+    static void unregister_screen_resize_hook(Signal<>::RegisterHandle handle);
+
+private:
+    /// Invoked by resize().
+    static Signal<> on_screen_resized;
 };
 
 }
--- src/detail/screen/composerscreen.cpp
+++ src/detail/screen/composerscreen.cpp
@@ -13,6 +13,8 @@ inline namespace v1
 namespace detail
 {
 
+Signal<> ComposerScreen::on_screen_resized;
+
 ComposerScreen::ComposerScreen(const Size& size)
 {
     detail::info("Composer Screen");
@@ -25,12 +27,28 @@ ComposerScreen::ComposerScreen(const Size& size)
 void ComposerScreen::resize(const Size& size)
 {
     if (m_size != size)
+    {
         init(size);
+
+        on_screen_resized.invoke();
+    }
 }
 
 unsigned char* ComposerScreen::get_pixmap()
 {
     return cairo_image_surface_get_data(m_surface.get());
+}
+
+
+Signal<>::RegisterHandle ComposerScreen::register_screen_resize_hook(const Signal<>::EventCallback& handler)
+{
+    return on_screen_resized(handler);
+}
+
+void ComposerScreen::unregister_screen_resize_hook(Signal<>::RegisterHandle handle)
+{
+    if (handle != Signal<>::INVALID_HANDLE)
+        on_screen_resized.remove(handle);
 }
 
 }
