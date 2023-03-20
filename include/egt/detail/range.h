ff953aa1892d509091aaa9987e1514f4926379e8
Ludovic Desroches <ludovic.desroches@microchip.com>
2023-03-20 13:16:05 +0100

    introduce Range utility class
    Introduce the Range utility class. Its purpose is to provide an iterator
    pair (begin, end) which represents a subset of a container.
    
    It seems C++20 introduces std::ranges but we are using C++17...
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>

---
+++ include/egt/detail/range.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2023 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef EGT_DETAIL_RANGE_H
+#define EGT_DETAIL_RANGE_H
+
+/**
+ * @file
+ * @brief Range class.
+ */
+
+namespace egt
+{
+inline namespace v1
+{
+namespace detail
+{
+
+/**
+ * Utility class to allow range loops from a subset of a container.
+ */
+template<typename T>
+class Range
+{
+public:
+    Range() {}
+
+    void begin(typename T::iterator begin) noexcept
+    {
+        m_begin = begin;
+    }
+
+    typename T::iterator begin() noexcept
+    {
+        return m_begin;
+    }
+
+    typename T::const_iterator begin() const noexcept
+    {
+        return m_begin;
+    }
+
+    typename T::const_iterator cbegin() const noexcept
+    {
+        return m_begin;
+    }
+
+    void end(typename T::iterator end) noexcept
+    {
+        m_end = end;
+    }
+
+    typename T::iterator end() noexcept
+    {
+        return m_end;
+    }
+
+    typename T::const_iterator end() const noexcept
+    {
+        return m_end;
+    }
+
+    typename T::const_iterator cend() const noexcept
+    {
+        return m_end;
+    }
+
+    size_t size() const noexcept
+    {
+        return std::distance(m_begin, m_end);
+    }
+
+    bool empty() const noexcept
+    {
+        return (m_begin == m_end);
+    }
+private:
+    typename T::iterator m_begin;
+    typename T::iterator m_end;
+};
+
+}
+}
+}
+
+#endif
