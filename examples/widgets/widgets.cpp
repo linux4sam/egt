634cd25e1fdbfcdcf3aa16572502a8bc0e088846
Ludovic Desroches <ludovic.desroches@microchip.com>
2024-02-28 08:51:06 +0100

    examples/widgets: rework the layout of the listbox page
    Rework the layout of the listbox page to have it nicely displayed in
    portrait mode.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    Acked-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>

--- examples/widgets/widgets.cpp
+++ examples/widgets/widgets.cpp
@@ -550,29 +550,39 @@ struct ComboPage : public egt::NotebookTab
 
 struct ListPage : public egt::NotebookTab
 {
-    ListPage()
+    ListPage(bool landscape, const egt::Size& tab_size)
     {
+        egt::Size content_size;
+        if (landscape)
+            content_size = tab_size * 0.9;
+        else
+            content_size = egt::Size(tab_size.width() * 0.9, tab_size.height() * 0.7);
         auto hsizer1 = std::make_shared<egt::BoxSizer>(egt::Orientation::horizontal,
                        egt::Justification::justify);
-        hsizer1->padding(20);
-        add(egt::expand(hsizer1));
+        hsizer1->resize(content_size);
+        add(egt::center(hsizer1));
 
-        auto list0 = std::make_shared<egt::ListBox>(egt::Rect(0, 0, 200, 0));
+        auto list0 = std::make_shared<egt::ListBox>();
         for (auto x = 0; x < 25; x++)
             list0->add_item(std::make_shared<egt::StringItem>("item " + std::to_string(x)));
-        list0->align(egt::AlignFlag::expand_vertical);
+        list0->resize({content_size.width() / 3, content_size.height() });
+        list0->margin(5);
         hsizer1->add(list0);
 
-        auto list1 = std::make_shared<egt::ListBox>(egt::Rect(0, 0, 200, 300));
+        auto list1 = std::make_shared<egt::ListBox>(egt::Rect(0, 0, 0, 300));
         for (auto x = 0; x < 5; x++)
             list1->add_item(std::make_shared<egt::StringItem>("item " + std::to_string(x), egt::Image("icon:ok.png")));
+        list1->width(content_size.width() / 3);
+        list1->margin(5);
         hsizer1->add(list1);
 
-        auto list2 = std::make_shared<egt::ListBox>(egt::Rect(0, 0, 200, 0));
+        auto list2 = std::make_shared<egt::ListBox>(egt::Rect(0, 0, 0, 0));
         list2->add_item(std::make_shared<egt::StringItem>("Help", egt::Image("icon:help.png")));
         list2->add_item(std::make_shared<egt::StringItem>("Info", egt::Image("icon:info.png")));
         list2->add_item(std::make_shared<egt::StringItem>("Warning", egt::Image("icon:warning.png")));
-        list2->height((list2->border() * 2) + (40 * list2->item_count()));
+        list2->margin(5);
+        list2->height((list2->moat() * 2) + (40 * list2->item_count()));
+        list2->width(content_size.width() / 3);
         hsizer1->add(list2);
     }
 };
@@ -779,7 +789,7 @@ int main(int argc, char** argv)
         {"Sliders", std::make_shared<SliderPage>(landscape, tab_size)},
         {"Meters", std::make_shared<MeterPage>(landscape, tab_size)},
         {"ComboBox", std::make_shared<ComboPage>()},
-        {"ListBox", std::make_shared<ListPage>()},
+        {"ListBox", std::make_shared<ListPage>(landscape, tab_size)},
         {"Scrollwheel", std::make_shared<ScrollwheelPage>()},
         {"Shapes", std::make_shared<ShapesPage>()},
     };
